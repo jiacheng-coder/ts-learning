f0e6ad4c40fd48d0155222f8890eb45b4a40da1f
zhujiacheng06 <zhujiacheng06@meituan.com>
2023-08-16 10:43:24 +0800

    init

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 let obj = {
-  1: 'bart',
-  2: 'lucy'
-}
-let arr = Object.keys(obj)
-arr.forEach(item=>{
+  1: "bart",
+  2: "lucy",
+};
+let arr = Object.keys(obj);
+arr.forEach((item) => {
   console.log(typeof item);
-})
+});
